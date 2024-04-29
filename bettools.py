350b1fd73e7c6b06befa7a4a8d8fdc2ed6fc4f32
Thom9 <aidan.mcgahey@hotmail.co.uk>
2024-04-29 13:42:47 +0100

    ran black, isort - autoformatting
    Runs black and isort
    
    pip install black
    pip install isort
    
    Greatly helps with the readability.

--- bettools.py
+++ bettools.py
@@ -1,7 +1,8 @@
-import pandas as pd
 import numpy as np
-from scipy.stats import poisson
+import pandas as pd
 from scipy.optimize import minimize
+from scipy.stats import poisson
+
 
 def generate_seasons(start_year, end_year):
     seasons = []
@@ -11,70 +12,103 @@ def generate_seasons(start_year, end_year):
         seasons.append(start + end)
     return seasons
 
+
 def get_data(season_list, league_list, additional_cols=[]):
-    
-    col_list=['Div','Date','HomeTeam','AwayTeam','FTHG','FTAG','PSH','PSD','PSA','home_max_odds','away_max_odds','draw_max_odds']
-    
+    col_list = [
+        "Div",
+        "Date",
+        "HomeTeam",
+        "AwayTeam",
+        "FTHG",
+        "FTAG",
+        "PSH",
+        "PSD",
+        "PSA",
+        "home_max_odds",
+        "away_max_odds",
+        "draw_max_odds",
+    ]
+
     for col in additional_cols:
         col_list.append(col)
 
     df_ls = []
-    
-    bookmakers = ['B365','BW','IW','PS','WH','VC']
+
+    bookmakers = ["B365", "BW", "IW", "PS", "WH", "VC"]
 
     home_cols = []
     away_cols = []
     draw_cols = []
-    
+
     for book in bookmakers:
-        home_col = book + 'H'
+        home_col = book + "H"
         home_cols.append(home_col)
-    
+
     for book in bookmakers:
-        away_col = book + 'A'
+        away_col = book + "A"
         away_cols.append(away_col)
 
     for book in bookmakers:
-        draw_col = book + 'D'
+        draw_col = book + "D"
         draw_cols.append(draw_col)
-    
+
     for season in season_list:
         for league in league_list:
             try:
-                df = pd.read_csv(f'https://www.football-data.co.uk/mmz4281/{season}/{league}.csv')
+                df = pd.read_csv(
+                    f"https://www.football-data.co.uk/mmz4281/{season}/{league}.csv"
+                )
             except:
-                df = pd.read_csv(f'https://www.football-data.co.uk/mmz4281/{season}/{league}.csv', encoding='latin')
+                df = pd.read_csv(
+                    f"https://www.football-data.co.uk/mmz4281/{season}/{league}.csv",
+                    encoding="latin",
+                )
             try:
-                df["Date"] = pd.to_datetime(df["Date"],format="%d/%m/%y")
+                df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%y")
             except ValueError:
-                df["Date"] = pd.to_datetime(df["Date"],format="%d/%m/%Y")
+                df["Date"] = pd.to_datetime(df["Date"], format="%d/%m/%Y")
             existing_home_columns = [col for col in home_cols if col in df.columns]
             existing_away_columns = [col for col in away_cols if col in df.columns]
             existing_draw_columns = [col for col in draw_cols if col in df.columns]
-            
-            df['home_max_odds'] = df[existing_home_columns].max(axis=1)
-            df['away_max_odds'] = df[existing_away_columns].max(axis=1)
-            df['draw_max_odds'] = df[existing_draw_columns].max(axis=1)
-            
+
+            df["home_max_odds"] = df[existing_home_columns].max(axis=1)
+            df["away_max_odds"] = df[existing_away_columns].max(axis=1)
+            df["draw_max_odds"] = df[existing_draw_columns].max(axis=1)
+
             df = df[col_list]
             df_ls.append(df)
     return df_ls
 
+
 def calculate_poisson_match_outcomes(home_goals_expectation, away_goals_expectation):
     max_goals = 10
-    home_probabilities = [poisson.pmf(i, home_goals_expectation) for i in range(0, max_goals+1)]
-    away_probabilities = [poisson.pmf(i, away_goals_expectation) for i in range(0, max_goals+1)]
-    
-    home_win_prob = sum(home_probabilities[i] * sum(away_probabilities[:i]) for i in range(1, max_goals+1))
-    draw_prob = sum(home_probabilities[i] * away_probabilities[i] for i in range(max_goals+1))
-    away_win_prob = sum(away_probabilities[i] * sum(home_probabilities[:i]) for i in range(1, max_goals+1))
-    
+    home_probabilities = [
+        poisson.pmf(i, home_goals_expectation) for i in range(0, max_goals + 1)
+    ]
+    away_probabilities = [
+        poisson.pmf(i, away_goals_expectation) for i in range(0, max_goals + 1)
+    ]
+
+    home_win_prob = sum(
+        home_probabilities[i] * sum(away_probabilities[:i])
+        for i in range(1, max_goals + 1)
+    )
+    draw_prob = sum(
+        home_probabilities[i] * away_probabilities[i] for i in range(max_goals + 1)
+    )
+    away_win_prob = sum(
+        away_probabilities[i] * sum(home_probabilities[:i])
+        for i in range(1, max_goals + 1)
+    )
+
     return [home_win_prob, draw_prob, away_win_prob]
+
 
 def calculate_ev_from_odds(bookmaker_odds, your_probability):
     payout = bookmaker_odds
     ev = (your_probability * payout) - 1
     return ev
+
 
 def find_best_fit_goals(prob_home_win, prob_draw, prob_away_win):
     """
@@ -84,55 +118,66 @@ def find_best_fit_goals(prob_home_win, prob_draw, prob_away_win):
     # Initial guesses for average goals scored by home and away teams
     avg_goals_home = 1.4
     avg_goals_away = 1.1
-    
+
     # Define a simple error function to minimize
     def error_function(guess):
         home, away = guess
         # Calculate win, draw, and lose probabilities using Poisson distribution
         max_goals = 10  # Maximum number of goals to consider for calculation
-        prob_draw_estimated = sum(poisson.pmf(i, home) * poisson.pmf(i, away) for i in range(max_goals))
-        prob_home_win_estimated = sum(poisson.pmf(i, home) * sum(poisson.pmf(j, away) for j in range(i)) for i in range(1, max_goals))
-        prob_away_win_estimated = sum(poisson.pmf(i, away) * sum(poisson.pmf(j, home) for j in range(i)) for i in range(1, max_goals))
-        
+        prob_draw_estimated = sum(
+            poisson.pmf(i, home) * poisson.pmf(i, away) for i in range(max_goals)
+        )
+        prob_home_win_estimated = sum(
+            poisson.pmf(i, home) * sum(poisson.pmf(j, away) for j in range(i))
+            for i in range(1, max_goals)
+        )
+        prob_away_win_estimated = sum(
+            poisson.pmf(i, away) * sum(poisson.pmf(j, home) for j in range(i))
+            for i in range(1, max_goals)
+        )
+
         # Error based on the difference between estimated and actual probabilities
-        error = ((prob_home_win_estimated - prob_home_win)**2 + 
-                 (prob_draw_estimated - prob_draw)**2 + 
-                 (prob_away_win_estimated - prob_away_win)**2)
+        error = (
+            (prob_home_win_estimated - prob_home_win) ** 2
+            + (prob_draw_estimated - prob_draw) ** 2
+            + (prob_away_win_estimated - prob_away_win) ** 2
+        )
         return error
-    
+
     # Use optimization to minimize the error function
     initial_guess = [avg_goals_home, avg_goals_away]
     result = minimize(error_function, initial_guess, bounds=((0, None), (0, None)))
-    
+
     if result.success:
         fitted_goals_home, fitted_goals_away = result.x
         return fitted_goals_home, fitted_goals_away
     else:
         return None
 
+
 def kelly_criterion(probability, odds, bankroll, kelly_fraction=1.0):
     """
     Calculate the optimal betting amount using the Kelly Criterion, with an option to use a fraction of the full recommendation.
-    
+
     Parameters:
     - probability: The probability of the outcome occurring.
     - odds: The decimal odds offered for the bet.
     - bankroll: The current amount in your bankroll.
     - kelly_fraction: Fraction of the Kelly bet to use (default is 1.0 for 100%).
-    
+
     Returns:
     - The optimal amount to bet from your bankroll, adjusted by the specified Kelly fraction.
     """
     b = odds - 1  # Convert decimal odds to b in the formula
     q = 1 - probability  # Probability of losing
-    
+
     # Calculate the fraction of the bankroll to bet, according to the Kelly Criterion
     f_star = (b * probability - q) / b
-    
+
     # Adjust the fraction with the specified Kelly fraction
     f_star = max(f_star, 0) * kelly_fraction
-    
+
     # Calculate the amount to bet
     bet_amount = f_star * bankroll
-    
+
     return bet_amount
